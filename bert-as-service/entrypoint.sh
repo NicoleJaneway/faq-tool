a81d06de909a79a68068521089dc1f48f1043643
Nicole Bills <nbills@atlasresearch.us>
2021-05-25 05:07:27 -0400

    update entrypoint with wget

--- bert-as-service/entrypoint.sh
+++ bert-as-service/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+apt-get update
+apt-get install wget
+wget https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
+unzip ./cased_L-12_H-768_A-12.zip
 bert-serving-start -model_dir=./cased_L-12_H-768_A-12 -num_worker=4 -max_seq_len=64 -cpu -http_port 8125
